8013ff0e4ea5aa5d5743f5bd976e64a908de3a3c
Aleksandr Semenov <semenlinux@gmail.com>
2021-04-21 10:50:08 +0300

    Add Search in Flat

---
+++ property/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Flat
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ["town", "address", "owner"]
+
+
+admin.site.register(Flat, AuthorAdmin)
