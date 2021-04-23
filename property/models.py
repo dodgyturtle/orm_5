b4a20fdcc875bc110b563ece5ff3b12a5f799b4f
Aleksandr Semenov <semenlinux@gmail.com>
2021-04-23 15:34:12 +0300

    Make pure phone

--- property/models.py
+++ property/models.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
+    owner_pure_phone = PhoneNumberField(null=True, blank=True)
     created_at = models.DateTimeField(
         "Когда создано объявление", default=timezone.now, db_index=True
     )
@@ -41,6 +43,7 @@ class Flat(models.Model):
     )
     new_building = models.NullBooleanField("Новостройка")
     liked_by = models.ManyToManyField(User, related_name="liked_flats")
+
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
